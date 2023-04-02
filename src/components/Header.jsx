3cd3ed719d91bf6523a85df704e559de533275fe
wesleyRenam <wesleyrenam@gmail.com>
2023-04-01 22:42:33 -0300

    Começando o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import '../css/header.css'
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="sidebar">
+      <img src={require("../imgs/perfil.jfif")} alt="minhafoto" className='foto-header' />
+      <Nav defaultActiveKey="/home" className="flex-column">
+        <Nav.Link href="/home">Home</Nav.Link>
+        <Nav.Link href="/about">Sobre</Nav.Link>
+        <Nav.Link href="/contact">Serviços</Nav.Link>
+        <Nav.Link href="/contact">Portfolio</Nav.Link>
+        <Nav.Link href="/contact">Contato</Nav.Link>
+      </Nav>
+    </div>
+  );
 }
 
 export default Header
